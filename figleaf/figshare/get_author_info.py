e4707c656d504d43e6528effc96e4a47a636be50
Virginia Scarlett <virginia.t.scarlett@gmail.com>
2023-04-11 10:44:29 -0400

    work on get_author_info; output is not pretty, but works

--- figleaf/figshare/get_author_info.py
+++ figleaf/figshare/get_author_info.py
@@ -5,16 +5,31 @@ parser = argparse.ArgumentParser(description='A script to grab author informatio
 parser.add_argument('-t', 
                     '--token', 
                     required = True, 
-                    help = 'Personal token, most easily obtained through the figshare website.',
+                    help = 'Personal token, most easily obtained through the figshare website.'
+                    )
+parser.add_argument('-a', 
+                    '--authors', 
+                    required = True, 
+                    help = 'Comma-separated list of author FULL names. Spaces are allowed.',
+                    type=str,
+                    nargs='+'
                     )
 args = parser.parse_args()
 
-url = 'https://api.figshare.com/v2/account/authors/search?limit=10000'
-headers = {
-     'Authorization': 'token {}'.format(args.token)
-     }
+full_names = [e.strip() for e in " ".join(args.authors).split(',')]
+
+#This next line will need to be changed if we ever have more than 1000 users. Note limit=10000 fails.
+url = 'https://api.figshare.com/v2/account/authors/search?limit=1000'
+headers = {'Authorization': 'token {}'.format(args.token)}
 
 response = requests.post(url, headers=headers)
 response.raise_for_status()
+#response.status_code
+#response.reason
+data = response.json() # a list of dicts
+final = []
+for author in data:
+    if author['full_name'] in full_names:
+        final.append(author)
 
-#TODO: filter json author list to contain only the authors you want
+print(final)
