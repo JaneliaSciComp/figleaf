3a212d7938a249d40c8c37e41ec5e16907a946fa
Virginia Scarlett <virginia.t.scarlett@gmail.com>
2023-05-12 15:18:20 -0400

    make create_private_article.py interactive and also create publish_article.py

--- figleaf/figshare/create_private_article.py
+++ figleaf/figshare/create_private_article.py
@@ -1,3 +1,9 @@
+"""
+A script to create a private article from a file containing JSON-formatted metadata for that article.
+Offers the user the option to upload one data file to that article. 
+Articles are private before being made public with publish_article.py
+"""
+
 import requests
 import argparse
 import json
@@ -5,20 +11,21 @@ import sys
 import hashlib
 
 CHUNK_SIZE = 10 * (1024 ** 2) # e.g. 10 * (1024 ** 2) = 10 Mb.
-FILE_NAME = 'AA1121.json'
+# For bug testing: my JSON-formatted metadata file is researcher_metadata.json
 
-def check(response_to_check):
-    if response_to_check.status_code != requests.codes.ok:
+def checkOK(response_to_check):
+    if not response_to_check.ok:
+    #if response_to_check.status_code != requests.codes.ok:
         print(f"Request failed with error code {response_to_check.status_code}")
         print(response_to_check.text) 
         sys.exit()
 
-def initiate_new_upload(article_id, file_name):
-    endpoint = '{}/{}/files'.format(base_url, article_id)
+def initiate_new_upload(url, headers, article_id, file_name):
+    endpoint = '{}/{}/files'.format(url, article_id)
     md5, size = get_file_check_data(file_name)
     file_data = {'name': file_name, 'md5': md5, 'size': size}
     upload_response = requests.post(endpoint, headers=headers, data=json.dumps(file_data))
-    check(upload_response)
+    checkOK(upload_response)
     return json.loads(upload_response.content) 
 
 def get_file_check_data(file_name):
@@ -32,13 +39,13 @@ def get_file_check_data(file_name):
             data = fin.read(CHUNK_SIZE)
         return md5.hexdigest(), size
 
-def upload_parts(file_info):
+def upload_parts(headers, file_info, file_name):
     res = requests.get(file_info['location'], headers=headers)
-    check(res)
+    checkOK(res)
     up_url, up_token = json.loads(res.content)['upload_url'], json.loads(res.content)['upload_token']
     uploader_service_response = requests.get(up_url, headers = {'Authorization': 'token {}'.format(up_token)} )
-    check(uploader_service_response)
-    with open(FILE_NAME, 'rb') as fin:
+    checkOK(uploader_service_response)
+    with open(file_name, 'rb') as fin:
         for part in json.loads(uploader_service_response.content)['parts']:
             upload_part(file_info, fin, part, up_url)
 
@@ -50,36 +57,40 @@ def upload_part(file_info, stream, part, up_url):
     stream.seek(part['startOffset'])
     part_data = stream.read(part['endOffset'] - part['startOffset'] + 1)
     part_res = requests.put(part_url, data=part_data)
-    check(part_res)
+    checkOK(part_res)
 
-def complete_upload(article_id, file_id):
-    requests.post('{}/{}/files/{}'.format(base_url, article_id, file_id))
-
-
+#def complete_upload(url, article_id, file_id):
+#    requests.post('{}/{}/files/{}'.format(url, article_id, file_id))
 
 
-parser = argparse.ArgumentParser(description='A script to create a new private figshare article, with metadata only. Research item will be uploaded later. Token required as a command line argument.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A script to create a new private figshare article, with metadata only. Research item will be uploaded later. Token required as a command line argument.')
+    parser.add_argument('-t', 
+                        '--token', 
+                        required = True, 
+                        help = 'Personal token, most easily obtained through the figshare website.'
+                        )
+    args = parser.parse_args()
 
-parser.add_argument('-t', 
-                    '--token', 
-                    required = True, 
-                    help = 'Personal token, most easily obtained through the figshare website.'
-                    )
+    metadata_file = input("Please enter the name of the JSON-formatted metadata file: ")
+    data = open(metadata_file, "rb").read()
+    base_url = "https://api.figsh.com/v2/account/articles"
+    headers = {'Authorization': 'token {}'.format(args.token)}
+    response = requests.post(base_url, headers=headers, data=data)
+    checkOK(response)
+    print(f"New private article with ID {json.loads(response.content)['entity_id']} successfully created from {metadata_file}.")
 
-args = parser.parse_args()
+    proceed = input(f"Would you like to upload a file to the article you just created? (y/n): ")
+    if proceed.lower() == "y":
+        file_to_upload = input("Please enter the name of the file to upload: ")
+        print("Uploading file ", file_to_upload, f"in {CHUNK_SIZE/(1024 ** 2)}Mb chunks")
+        file_info = initiate_new_upload(base_url, headers, json.loads(response.content)['entity_id'], file_to_upload)
+        # Until here we used the figshare API; the following lines use the figshare upload service API.
+        upload_parts(headers, file_info, file_to_upload) # looks like e.g. {'location': 'https://api.figsh.com/v2/account/articles/8417838/files/830411224'}
+        # complete the upload
+        requests.post(file_info['location'], headers=headers)
+    else:
+        print("Exiting.")
+        sys.exit()
 
-# Note that researcher_metadata.json should be in the same directory as this script.
-data = open("researcher_metadata.json", "rb").read()
-base_url = "https://api.figsh.com/v2/account/articles"
-headers = {'Authorization': 'token {}'.format(args.token)}
-response = requests.post(base_url, headers=headers, data=data)
-check(response)
-
-# TODO: Ask the user if they want to proceed to upload a data file, and if so, name of file
-
-file_info = initiate_new_upload(json.loads(response.content)['entity_id'], FILE_NAME)
-
-# Until here we used the figshare API; following lines use the figshare upload service API.
-upload_parts(file_info) # looks like e.g. {'location': 'https://api.figsh.com/v2/account/articles/8417838/files/830411224'}
-# complete the upload
-requests.post(file_info['location'], headers=headers)
+    print("Upload complete.")
---
+++ figleaf/figshare/publish_article.py
@@ -0,0 +1,60 @@
+"""
+A script to make a private article public, given an article id.
+Article id can only be retrieved through the figshare API.
+Run this script after create_private_article.py (which prints the article_id to the terminal, btw).
+Offers the user the option to reserve a DOI for the article and to
+upload additional data files.
+"""
+
+import requests
+import argparse
+import create_private_article
+
+CHUNK_SIZE = 10 * (1024 ** 2) # e.g. 10 * (1024 ** 2) = 10 Mb.
+
+parser = argparse.ArgumentParser(description='A script to modify and/or publish a private figshare article with known article id.')
+
+parser.add_argument('-id', 
+                    '--article_id', 
+                    required = True, 
+                    help = 'Personal token, most easily obtained through the figshare website.'
+                    )
+parser.add_argument('-t', 
+                    '--token', 
+                    required = True, 
+                    help = 'Personal token, most easily obtained through the figshare website.'
+                    )
+args = parser.parse_args()
+
+headers = {'Authorization': 'token {}'.format(args.token)}
+base_url = "https://api.figsh.com/v2/account/articles"
+
+doi = input('Would you like to reserve a DOI for this article? (y/n): ')
+if doi.lower() == 'y':
+    doi_res = requests.post(f"https://api.figsh.com/v2/account/articles/{args.article_id}/reserve_doi", headers=headers)
+    create_private_article.checkOK(doi_res)
+    print("DOI reserved successfully.")
+
+while True:
+    proceed = input("Would you like to upload an additional file to this article? (y/n): ")
+    if proceed.lower() == "y":
+        file_to_upload = input("Please enter the name of the file to upload: ")
+        print("Uploading file ", file_to_upload, f"in {CHUNK_SIZE/(1024 ** 2)}Mb chunks")
+        file_info = create_private_article.initiate_new_upload(base_url, headers, args.article_id, file_to_upload)
+        create_private_article.upload_parts(headers, file_info, file_to_upload) 
+        up_res = requests.post(file_info['location'], headers=headers)
+        create_private_article.checkOK(up_res)
+        print("Upload successful.")
+    elif proceed.lower() == "n":
+        break  # break out of the while loop
+    else:
+        print("Invalid input. Please enter 'y' to upload a file, or 'n' to cancel.")
+
+publish = input(f"Would you like to publish this article now? (y/n): ")
+if publish.lower() == 'y':
+    pub_res = requests.post(f"https://api.figsh.com/v2/account/articles/{args.article_id}/publish", headers=headers)
+    create_private_article.checkOK(pub_res)
+    print("Article published successfully.")
+
+
+
